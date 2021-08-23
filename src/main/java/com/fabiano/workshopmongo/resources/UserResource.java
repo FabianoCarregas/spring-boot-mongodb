351f6a7a5d4c0949960e5e8f7a28937becadcff6
unknown <fabianofac_@hotmail.com>
2021-08-23 14:50:40 -0300

    returning users by dto

--- src/main/java/com/fabiano/workshopmongo/resources/UserResource.java
+++ src/main/java/com/fabiano/workshopmongo/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.fabiano.workshopmongo.resources;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fabiano.workshopmongo.domain.User;
+import com.fabiano.workshopmongo.dto.UserDTO;
 import com.fabiano.workshopmongo.services.UserService;
 
 @RestController
@@ -21,9 +23,10 @@ public class UserResource {
 	
 
 	@RequestMapping(method= RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findall();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto= list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 		
 		
 		
