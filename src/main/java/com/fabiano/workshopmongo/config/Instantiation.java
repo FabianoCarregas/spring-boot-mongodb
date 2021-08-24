fa5992c8abe6a5b621fec02b2fc4ce6b43bb7a1f
unknown <fabianofac_@hotmail.com>
2021-08-24 15:39:08 -0300

    post comments

--- src/main/java/com/fabiano/workshopmongo/config/Instantiation.java
+++ src/main/java/com/fabiano/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.fabiano.workshopmongo.domain.Post;
 import com.fabiano.workshopmongo.domain.User;
 import com.fabiano.workshopmongo.dto.AuthorDTO;
+import com.fabiano.workshopmongo.dto.CommentDTO;
 import com.fabiano.workshopmongo.repository.PostRepository;
 import com.fabiano.workshopmongo.repository.UserRepository;
 
@@ -43,10 +44,19 @@ public class Instantiation implements CommandLineRunner {
 		Post post1= new Post(null, sdf.parse("21/02/2021"),"let's go to the trip!!", "I'm going to travel to Spain",new AuthorDTO(maria));
 		Post post2= new Post(null, sdf.parse("23/02/2021"),"Good morning", "today is a beautiful day",new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Have a good trip mate!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Enjoy", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Nice mate", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c3));
+		post2.getComments().add(c2);
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
+		
+		
 		
 	}
 	
