a5a588ee357167f4e8e92afb9e5d2d1e69b692ab
unknown <fabianofac_@hotmail.com>
2021-08-25 16:44:37 -0300

    get coment by date and by coment

--- src/main/java/com/fabiano/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/fabiano/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.fabiano.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -15,6 +16,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	List<Post> findByTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
-		
+	
+	@Query("{ $and: [{date: {$gte: ?1}}, { date: { $lte: ?2} } , { $or: [ {'title': {$regex: ?0, $options: '<options>'} }, {'body': {$regex: ?0, $options: '<options>'}},{'comments.text': {$regex: ?0, $options: '<options>'}} ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
