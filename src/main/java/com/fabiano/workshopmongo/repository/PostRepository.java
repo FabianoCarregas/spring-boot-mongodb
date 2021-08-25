4722231150075b9d228c1277d4750048e97e7626
unknown <fabianofac_@hotmail.com>
2021-08-25 17:17:29 -0300

    simple search bu query

--- src/main/java/com/fabiano/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/fabiano/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.fabiano.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fabiano.workshopmongo.domain.Post;
@@ -10,7 +11,8 @@ import com.fabiano.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
-	
+	@Query("{'title': {$regex: ?0, $options: '<options>'}}")
+	List<Post> findByTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 		
