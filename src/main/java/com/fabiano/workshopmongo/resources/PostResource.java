8b78c06f0bf7c64eb6ef1fb19f246d470535bfde
unknown <fabianofac_@hotmail.com>
2021-08-25 10:33:50 -0300

    query methods research

--- src/main/java/com/fabiano/workshopmongo/resources/PostResource.java
+++ src/main/java/com/fabiano/workshopmongo/resources/PostResource.java
@@ -1,14 +1,18 @@
 package com.fabiano.workshopmongo.resources;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fabiano.workshopmongo.domain.Post;
+import com.fabiano.workshopmongo.resources.util.URL;
 import com.fabiano.workshopmongo.services.PostService;
 
 @RestController
@@ -18,9 +22,6 @@ public class PostResource {
 	@Autowired
 	private PostService service;
 	
-
-	
-	
 	@RequestMapping(value="/{id}", method= RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		Post obj = service.findById(id);
@@ -28,6 +29,12 @@ public class PostResource {
 		
 	}
 	
+	@RequestMapping(value="/titlesearch", method= RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue="") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	
+	}
 
 }
